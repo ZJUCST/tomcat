a1217f6a135b59652425ffc68354a26bc5edd6fd
zmzsmnh <zmzsmnh@gmail.com>
2013-03-02 14:08:53 +0800

    initial halotomcat project

---
+++ HaloTomcat/src/com/haloword/StaticResourceProcessor.java
@@ -0,0 +1,15 @@
+package com.haloword;
+
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+
+  public void process(Request request, Response response) {
+    try {
+      response.sendStaticResource();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ HaloTomcat/src/com/haloword/startup/Bootstrap.java
@@ -0,0 +1,10 @@
+package com.haloword.startup;
+
+import com.haloword.connector.http.HttpConnector;
+
+public final class Bootstrap {
+  public static void main(String[] args) {
+    HttpConnector connector = new HttpConnector();
+    connector.start();
+  }
+}
